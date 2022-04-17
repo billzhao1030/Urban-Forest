147b3860020cc60c3e2ec6557857fd7b0bc9fffb
billzhao <billzhao1030@gmail.com>
2022-04-17 16:28:14 +1000

    extract widgets

---
+++ lib/utils/string_validate.dart
@@ -0,0 +1,22 @@
+extension ValidateString on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidUserName{
+    final nameRegExp = RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidPassword{
+    final passwordRegExp = 
+    RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,15a}/pre>');
+    return passwordRegExp.hasMatch(this);
+  }
+
+  bool get isValidPhone{
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(this);
+  }
+}
